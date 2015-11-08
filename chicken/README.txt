1daf5ee7a9b3717ea8e7a77d354f1e1bab709c93
BlockMen <nmuelll@web.de>
2015-11-08 18:34:34 +0100

    Add chicken

---
+++ chicken/README.txt
@@ -0,0 +1,26 @@
+Chicken for Creatures MOB-Engine
+================================
+Copyright (c) 2015 BlockMen <blockmen2015@gmail.com>
+
+Version: 2.0 Beta
+
+
+Adds chicken to Minetest (requires Creatures MOB-Engine).
+Chicken spawn on dirt and grass blocks, have 5 HP and friendly. When killed they
+drop raw meat.
+
+
+License:
+~~~~~~~~
+Code:
+(c) Copyright 2015 BlockMen; modified zlib-License
+see "LICENSE.txt" for details.
+
+Media(textures and meshes/models):
+(c) Copyright (2014-2015) BlockMen; CC-BY-SA 3.0
+
+Sounds:
+- creatures_chicken*.ogg, dobroide(https://freesound.org/people/dobroide) CC BY 3.0
+Github:
+~~~~~~~
+https://github.com/BlockMen/cme/chicken
---
+++ chicken/init.lua
@@ -0,0 +1,92 @@
+--= Chicken for Creatures MOB-Engine (cme) =--
+-- Copyright (c) 2015 BlockMen <blockmen2015@gmail.com>
+--
+-- init.lua
+--
+-- This software is provided 'as-is', without any express or implied warranty. In no
+-- event will the authors be held liable for any damages arising from the use of
+-- this software.
+--
+-- Permission is granted to anyone to use this software for any purpose, including
+-- commercial applications, and to alter it and redistribute it freely, subject to the
+-- following restrictions:
+--
+-- 1. The origin of this software must not be misrepresented; you must not
+-- claim that you wrote the original software. If you use this software in a
+-- product, an acknowledgment in the product documentation is required.
+-- 2. Altered source versions must be plainly marked as such, and must not
+-- be misrepresented as being the original software.
+-- 3. This notice may not be removed or altered from any source distribution.
+--
+
+
+
+local def = {
+  -- general
+  name = "creatures:chicken",
+  stats = {
+    hp = 5,
+    lifetime = 300, -- 5 Minutes
+    can_jump = 1,
+    can_swim = true,
+    can_burn = true,
+		can_panic = true,
+    has_kockback = true,
+    sneaky = true,
+  },
+
+  modes = {
+    idle = {chance = 0.25, duration = 5, update_yaw = 3},
+    idle2 = {chance = 0.3, duration = 1},
+    pick = {chance = 0.25, duration = 2},
+    walk = {chance = 0.2, duration = 5.5, moving_speed = 0.7, update_yaw = 2},
+    panic = {moving_speed = 2.1}
+  },
+
+  model = {
+    mesh = "creatures_chicken.b3d",
+    textures = {"creatures_chicken.png"},
+    collisionbox = {-0.25, -0.01, -0.3, 0.25, 0.45, 0.3},
+    rotation = -90.0,
+    animations = {
+      idle = {start = 0, stop = 1, speed = 10},
+      idle2 = {start = 41, stop = 61, speed = 70},
+      pick = {start = 103, stop = 155, speed = 50},
+      walk = {start = 3, stop = 38, speed = 50},
+      -- special modes
+      swim = {start = 42, stop = 102, speed = 40},
+      panic = {start = 42, stop = 102, speed = 55},
+      death = {start = 156, stop = 176, speed = 28, loop = false, duration = 2.12},
+    },
+  },
+
+  sounds = {
+      on_damage = {name = "creatures_chicken_hit", gain = 0.5, distance = 10},
+      on_death = {name = "creatures_chicken_hit", gain = 0.5, distance = 10},
+      swim = {name = "creatures_splash", gain = 1.0, distance = 10},
+      random = {
+        idle = {name = "creatures_chicken", gain = 0.9, distance = 12, time_min = 8, time_max = 50},
+      },
+  },
+
+  spawning = {
+    abm_nodes = {
+      spawn_on = {"default:dirt_with_grass", "default:dirt"},
+    },
+    abm_interval = 55,
+    abm_chance = 7800,
+    max_number = 1,
+    number = 1,
+    light = {min = 8, max = 15},
+    height_limit = {min = 0, max = 150},
+
+    spawn_egg = {
+      description = "Chicken Spawn-Egg",
+    --  texture = "creatures_spawn_egg.png",
+    },
+
+  },
+
+}
+
+creatures.register_mob(def)
